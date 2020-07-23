625e48e78dc4883935ffb6591815f7ccea3e0674
Shirley Wu <shirleyswwu@gmail.com>
2020-07-23 08:19:10 -0700

    skeleton for api promise chaining exercise

--- exercises/bare_minimum/basicChaining.js
+++ exercises/bare_minimum/basicChaining.js
@@ -12,9 +12,31 @@ var fs = require('fs');
 var Promise = require('bluebird');
 
 
-
 var fetchProfileAndWriteToFile = function(readFilePath, writeFilePath) {
   // TODO
+  var readUsername = function(readFilePath) {
+    return new Promise((resolve, reject) => {
+      fs.readFile(readFilePath, 'utf8',(err, userName) => {
+        if (err) reject(err)
+        let lines = userName.split('\n');
+        resolve(lines[0])
+    });
+  }
+  // the next .then should use fs.writeFile, i would need a function on its own and call it inside .then
+  var writeToFile = function(writeFilePath) {
+
+  };
+  // then sends a request to the GitHub API for the user's profile
+  // using request(userProfileUrl, (err, resp)...)
+  var sendRequest = function() {
+    ;
+  }
+
+  readUsername(readFilePath);
+  .then(sendRequest)
+  .then(writeToFile)
+  ,catch();
+  });
 };
 
 // Export these functions so we can test them
