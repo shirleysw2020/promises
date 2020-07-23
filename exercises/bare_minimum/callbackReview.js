db44b6f68d1c4d5211f237bfd51d657c98809abf
Shirley Wu <shirleyswwu@gmail.com>
2020-07-22 23:59:18 -0700

    pass readfile and get status code in callback and promsie methods

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,27 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = (filePath, callback) => {
+  fs.readFile(filePath, 'utf8', (err, content) => {
+    if (err) {
+      callback(err, null);
+      // done('fs.readFile failed : (/n', err);
+    } else {
+      let split = content.split('\n');
+      callback(null, split[0]);
+      // callback('fs.readFile successfully completed :)\n', content);
+    }
+  });
 };
-
-// This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+// Retrieve the status code of a GET request to `url`
+var getStatusCode = (url, callback) => {
+  request(url, (err, resp) => {
+    if (err) {
+      callback(err);
+    } else {
+      callback(err, resp.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
--- exercises/bare_minimum/promiseConstructor.js
+++ exercises/bare_minimum/promiseConstructor.js
@@ -7,15 +7,30 @@
 var fs = require('fs');
 var request = require('request');
 var Promise = require('bluebird');
+// we can use promise promisify when the node fn conform to node.js convention of accepting a cb as last arg and calling the cb with err as 1st arg and success value as 2nd arg.
+// Promise.promisifyAll(readFile)
 
 // This function should retrieve the first line of the file at `filePath`
 var pluckFirstLineFromFileAsync = function(filePath) {
-  // TODO
+  // make a promise
+  return new Promise((resolve, reject) => {
+    // read file
+    fs.readFile(filePath, 'utf8', (err, content) => {
+      if (err) return reject(err);
+      let split = content.split('\n');
+      resolve(split[0]);
+    });
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
 var getStatusCodeAsync = function(url) {
-  // TODO
+  return new Promise((resolve, reject) => {
+    request(url, (err, resp) => {
+      if (err) return reject(err);
+      resolve(resp.statusCode);
+    });
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
